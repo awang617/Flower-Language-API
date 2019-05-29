40725dbe04006e70b82e99f3b618b33faf5b70e7
awang617 <wanga18@mail.wlu.edu>
2019-05-29 10:41:33 -0700

    setting up server file

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// allow cross origin requests (optional)
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+const db = require('./models');
+
+// Serve static files from the `/public` directory:
+// i.e. `/images`, `/scripts`, `/styles`
+app.use(express.static('public'));
+
+/*
+ * HTML Endpoints
+ */
+//  route to serve landing page
+app.get('/', function homepage(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+  });
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on the port that Heroku prescribes (process.env.PORT) OR port 3000
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Express server is up and running on http://localhost:3000/');
+});
